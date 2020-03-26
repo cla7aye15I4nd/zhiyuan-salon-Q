2b3b93454b3dade54a4e693cfdc9598a710669dc
dataisland <dataisland@sjtu.edu.cn>
2020-03-26 17:15:57 +0800

    Create defines_default.py

---
+++ src/app/defines_default.py
@@ -0,0 +1,35 @@
+mail_title = '致远沙龙统计文件'
+sender_mail = 'dataisland@sjtu.edu.cn'
+sender_password = 'password'
+
+names = {
+    'dataisland@sjtu.edu.cn': '于峥',
+    'wjj328685@sjtu.edu.cn' : '王晶晶老师',
+    'xinhe@sjtu.edu.cn'     : '何欣老师',
+    'qluo_zy@sjtu.edu.cn'   : '罗勤老师',
+    'wangyanzy@sjtu.edu.cn' : '汪燕老师'
+}
+
+save_path = 'saves'
+zy_article_url = 'http://zhiyuan.sjtu.edu.cn/articles/'
+
+time_str = '%04d-%02d-%02d %02d:%02d:%02d'
+
+salon_records = [('2013', '2948'),
+                 ('2014', '2943'),
+                 ('2015', '2942'),
+                 ('2016', '1902'),
+                 ('2017', '2909'),
+                 ('2018', '2919'),
+                 ('2019', '3368'),
+		         ('2020', '3938'),
+                 ('other', '2397')]
+
+other_set = {'other'}
+
+stu_info_file = 'stu_info.csv'
+update_time_file = 'update_time.text'
+salon_record_file = 'record_%s.text'
+
+class UniObject(object):
+    pass
